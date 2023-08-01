cb34039a1a622b15fa2ebb6272c24fdad64c438d
Amit Goyal <agoyal3_be21@thapar.edu>
2023-08-01 21:58:15 +0530

    team generation logic optimised

--- src/Components/TeamGenerator/TeamGenerator.jsx
+++ src/Components/TeamGenerator/TeamGenerator.jsx
@@ -50,35 +50,91 @@ const TeamGenerator = () => {
   };
 
   const generateTeams = members => {
+    let isUniform = false;
+    let attempts = 0;
+    let teams;
+
+    while (!isUniform) {
+      attempts++;
+      teams = generateSingleSetOfTeams (members);
+      isUniform = checkUniformDistribution (teams);
+    }
+
+    console.log ('Teams:', teams);
+    console.log ('Is Distributed Uniformly:', isUniform);
+    console.log ('Number of Attempts:', attempts);
+
+    return teams;
+  };
+
+  const generateSingleSetOfTeams = members => {
     // Separate team leaders and regular members
     const leaders = members.filter (member => member.role === 'team leader');
     const regularMembers = members.filter (member => member.role === 'member');
 
-    // Shuffle team leaders and regular members
+    // Shuffle team leaders and regular members separately
     const shuffledLeaders = _.shuffle (leaders);
     const shuffledRegularMembers = _.shuffle (regularMembers);
 
     const teamSize = 3; // Change this to set the desired team size
     const numTeams = Math.ceil (members.length / teamSize);
     const teams = Array.from ({length: numTeams}, () => []);
-    let currentIndex = 0;
 
-    // Assign team leaders to each team
-    shuffledLeaders.forEach (leader => {
-      teams[currentIndex].push (leader);
-      currentIndex = (currentIndex + 1) % numTeams;
-    });
-
-    // Distribute regular members evenly across teams
-    shuffledRegularMembers.forEach (member => {
-      if (teams[currentIndex].length >= teamSize) {
-        currentIndex = (currentIndex + 1) % numTeams;
+    // Distribute team leaders across teams, ensuring one leader per team
+    for (let i = 0; i < numTeams; i++) {
+      if (shuffledLeaders.length > 0) {
+        teams[i].push (shuffledLeaders.pop ());
       }
-      teams[currentIndex].push (member);
-      currentIndex = (currentIndex + 1) % numTeams;
-    });
+    }
+
+    // Group regular members by department
+    const regularMembersByDepartment = _.groupBy (
+      shuffledRegularMembers,
+      'department'
+    );
+
+    // Calculate the maximum number of members each department can have in a team
+    const maxMembersPerDepartment = Math.floor (
+      shuffledRegularMembers.length / numTeams
+    );
+
+    // Distribute regular members evenly across teams, considering the department of the team leader
+    let teamIndex = 0;
+    for (const department of Object.keys (regularMembersByDepartment)) {
+      const departmentMembers = regularMembersByDepartment[department];
+      while (departmentMembers.length > 0) {
+        if (teams[teamIndex].length >= teamSize) {
+          teamIndex = (teamIndex + 1) % numTeams;
+        }
+
+        const member = departmentMembers.pop ();
+        teams[teamIndex].push (member);
+        teamIndex = (teamIndex + 1) % numTeams;
+
+        // To avoid adding too many members from the same department to a team
+        if (
+          teams[teamIndex].length < maxMembersPerDepartment &&
+          departmentMembers.length === 0
+        ) {
+          break;
+        }
+      }
+    }
 
     return teams;
+  };
+
+  const checkUniformDistribution = teams => {
+    const departments = ['dev', 'designing', 'ml'];
+    for (let i = 0; i < teams.length; i++) {
+      const teamDepartments = teams[i].map (member => member.department);
+      for (const department of departments) {
+        if (!teamDepartments.includes (department)) {
+          return false;
+        }
+      }
+    }
+    return true;
   };
 
   return (
